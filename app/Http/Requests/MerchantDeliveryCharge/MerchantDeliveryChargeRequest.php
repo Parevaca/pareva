fe5ace34100622318530c277c60e72d03167c078
Hasmukh8899 <jadavhasmukh10@gmail.com>
2024-05-08 23:00:38 +0530

    store merchant wise delivery charge with distance option

--- app/Http/Requests/MerchantDeliveryCharge/MerchantDeliveryChargeRequest.php
+++ app/Http/Requests/MerchantDeliveryCharge/MerchantDeliveryChargeRequest.php
@@ -5,16 +5,16 @@ namespace App\Http\Requests\MerchantDeliveryCharge;
 use App\Enums\Status;
 use App\Models\Backend\MerchantDeliveryCharge;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
 
-class MerchantDeliveryChargeRequest extends FormRequest
-{
+class MerchantDeliveryChargeRequest extends FormRequest {
+
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    public function authorize()
-    {
+    public function authorize() {
         return true;
     }
 
@@ -23,50 +23,71 @@ class MerchantDeliveryChargeRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
-    {
-        return [
-            'delivery_charge_id' => ['required', 'numeric'],
-            'same_day'           => ['required','numeric'],
-            'next_day'           => ['required','numeric'],
-            'sub_city'           => ['required','numeric'],
-            'outside_city'       => ['required','numeric'],
-            'status'             => ['required','numeric'],
-        ];
+    public function rules() {
+        if (Request::input('delivery_type') == 1) {
+            return [
+                'delivery_charge_id' => ['required', 'numeric'],
+                'delivery_type' => ['required', 'numeric'],
+                'distance_type' => ['required', 'numeric'],
+                'distance' => ['required', 'numeric'],
+                'distance_charge' => ['required', 'numeric'],
+                'status' => ['required', 'numeric'],
+            ];
+        }else{
+            return [
+                'delivery_charge_id' => ['required', 'numeric'],
+                'same_day' => ['required', 'numeric'],
+                'next_day' => ['required', 'numeric'],
+                'sub_city' => ['required', 'numeric'],
+                'outside_city' => ['required', 'numeric'],
+                'delivery_type' => ['required', 'numeric'],
+                'status' => ['required', 'numeric'],
+            ];
+        }
     }
 
-    public function attributes()
-    {
-        return [
-            'delivery_charge_id'         => trans('validation.attributes.delivery_category'),
-            'status'                     => trans('validation.attributes.status'),
-            'same_day'                   => trans('validation.attributes.same_day'),
-            'next_day'                   => trans('validation.attributes.next_day'),
-            'sub_city'                   => trans('validation.attributes.sub_city'),
-            'outside_city'               => trans('validation.attributes.outside_city'),
-        ];
+    public function attributes() {
+        if (Request::input('delivery_type') == 1) {
+            return [
+                'delivery_charge_id' => trans('validation.attributes.delivery_category'),
+                'status' => trans('validation.attributes.status'),
+                'delivery_type' => trans('validation.attributes.delivery_type'),
+                'distance_type' => trans('validation.attributes.distance_type'),
+                'distance' => trans('validation.attributes.distance'),
+                'distance_charge' => trans('validation.attributes.distance_charge'),
+            ];
+        }else{
+            return [
+                'delivery_charge_id' => trans('validation.attributes.delivery_category'),
+                'status' => trans('validation.attributes.status'),
+                'same_day' => trans('validation.attributes.same_day'),
+                'next_day' => trans('validation.attributes.next_day'),
+                'sub_city' => trans('validation.attributes.sub_city'),
+                'outside_city' => trans('validation.attributes.outside_city'),
+                'delivery_type' => trans('validation.attributes.delivery_type'),
+            ];
+        }
     }
 
-    public function withValidator($validator)
-    {
+    public function withValidator($validator) {
 
         $validator->after(function ($validator) {
-                if ($this->userUniqueCheck()) {
-                    $validator->errors()->add('delivery_charge_id', trans('validation.attributes.delivery_category'));
-                }
+            if ($this->userUniqueCheck()) {
+                $validator->errors()->add('delivery_charge_id', trans('validation.attributes.delivery_category'));
+            }
         });
     }
 
-    private function userUniqueCheck()
-    {
-        $id                                    = $this->id;
-        $queryArray['delivery_charge_id']      = request('delivery_charge_id');
-        $queryArray['merchant_id']             = $this->merchant;
-        $hubInCharge                           = MerchantDeliveryCharge::where($queryArray)->where('id', '!=', $id)->first();
+    private function userUniqueCheck() {
+        $id = $this->id;
+        $queryArray['delivery_charge_id'] = request('delivery_charge_id');
+        $queryArray['merchant_id'] = $this->merchant;
+        $hubInCharge = MerchantDeliveryCharge::where($queryArray)->where('id', '!=', $id)->first();
 
         if (blank($hubInCharge)) {
             return false;
         }
         return true;
     }
+
 }
--- public/backend/js/merchantDeliveryCharge/create.js
+++ public/backend/js/merchantDeliveryCharge/create.js
@@ -1,14 +1,28 @@
 "use strict";
-$(function()  {
+function showDeliveryLables(){
+    var type = $('#deliveryChargeID').find(":selected").attr('data-type');
+    $("#delivery_type").val(type);
+    if (type == 0) {
+        $("#distance_type,#range,#distance_charge").hide();
+        $("#city_same_day,#city_next_day,#city_sub_city,#city_outside,#weight_group").show();
+    } else if(type == 1){
+        $("#city_same_day,#city_next_day,#city_sub_city,#city_outside,#weight_group").hide();
+        $("#distance_type,#range,#distance_charge").show();
+    }else{
+        $("#city_same_day,#city_next_day,#city_sub_city,#city_outside,#weight_group,#distance_type,#range,#distance_charge").hide();
+    }
+}
+$(function () {
     var delivery_charge_id = $("select#deliveryChargeID option").filter(":selected").val();
-    if(delivery_charge_id){
+    if (delivery_charge_id) {
         $.ajax({
-            type : 'POST',
-            url : $('#deliveryChargeID').data('url'),
-            data : {'delivery_charge_id':delivery_charge_id},
-            dataType : "html",
-            success : function (data) {
+            type: 'POST',
+            url: $('#deliveryChargeID').data('url'),
+            data: {'delivery_charge_id': delivery_charge_id},
+            dataType: "html",
+            success: function (data) {
                 $('#deliveryChargeInfo').html(data);
+                showDeliveryLables();
             }
         });
     }
@@ -17,12 +31,25 @@ $(function()  {
 
 $(document).on('change', '#deliveryChargeID', function () {
     $.ajax({
-        type : 'POST',
-        url : $(this).data('url'),
-        data : {'delivery_charge_id': $(this).val()},
-        dataType : "html",
-        success : function (data) {
+        type: 'POST',
+        url: $(this).data('url'),
+        data: {'delivery_charge_id': $(this).val()},
+        dataType: "html",
+        success: function (data) {
             $('#deliveryChargeInfo').html(data);
+            showDeliveryLables();
         }
     });
 });
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57)){
+        if(charCode ==46){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
